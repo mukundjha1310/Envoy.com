9ffcafdc25ed16ddf337fe7c2e8fcc3d90005c83
deep1049 <103635801+deep1049@users.noreply.github.com>
2022-07-21 22:09:00 +0530

    day-3 done

---
+++ comdetails.js
@@ -0,0 +1,31 @@
+document.querySelector("form"),addEventListener("submit",compdata);
+let arr=[];
+function compdata(){
+    
+        let obj={
+            companyaddress:document.querySelector("#compaddress").value,
+            companyname:document.querySelector("#compname").value,
+             compansize:document.querySelector("#select").value
+        };
+        if(obj.length===0){
+                alert("please fill all Company data")
+        }
+        else{
+
+        arr.push(obj);
+        console.log(arr);
+        
+        alert("company data save");
+        localStorage.setItem("array",JSON.stringify(arr));
+        window.location.href="loginemail.html";
+      }  
+    
+}
+function sign(event){
+        event.preventDefault()
+        window.location.href="loginemail.html"
+}
+function signup(event){
+        event.preventDefault()
+        window.location.href="signup.html"
+}
