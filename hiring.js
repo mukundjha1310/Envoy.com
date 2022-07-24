67f5efceecf3b8d50b4dd3e557046841f63dcdcf
Mukund Jha <mukundjha1310@gmail.com>
2022-07-24 12:47:09 +0530

    updated

---
+++ hiring.js
@@ -0,0 +1,34 @@
+function setimage1(){
+        
+    document.getElementById("image").src="https://envoy.com/images/jobs/life-1.png";
+    document.getElementById("h1").innerText="It's the people who work here that make Envoy special. We welcome and embrace every personality, so show up as your true self and let's have some fun!";
+}
+function setimagea(){
+    document.getElementById("h1").innerText=" ";
+}
+function setimageb(){
+    document.getElementById("h2").innerText=" ";
+}
+function setimagec(){
+    document.getElementById("h3").innerText=" ";
+}
+// function setimaged(){
+//     document.getElementById("h4").innerText=" ";
+// }
+
+function setimage2(){
+   document.getElementById("image").src="https://envoy.com/images/jobs/life-2.png";
+   document.getElementById("h2").innerText="If you like small teams that make a big impact, then Envoy is right for you. Come join us to learn, grow your career, and contribute to something big.";
+}
+function setimage3(){
+   document.getElementById("image").src="https://envoy.com/images/jobs/life-3.png";
+   document.getElementById("h3").innerText="We build products that solve real problems for our customers. We love to contribute to creating a great workplace experience—both within our own offices and for our customer’s workplaces around the world.";
+}
+function setimage4(){
+   document.getElementById("image").src="https://envoy.com/images/jobs/life-4.png";
+   document.getElementById("h4").innerText="From Slack and Pandora, to Box, Yelp and beyond, millions of visitors and employees interact with Envoy everyday. We truly believe that we have the best customers and we love making them happy.";
+}
+function cnbc(event){
+  event.preventDefault()
+    window.location.href="https://www.cnbc.com/2022/05/17/these-are-the-2022-cnbc-disruptor-50-companies.html"
+}
